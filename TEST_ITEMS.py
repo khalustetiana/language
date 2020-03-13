a9f3627f74e90faa19ead74f9a9c2763aedbc0b1
khalustetiana <51445317+khalustetiana@users.noreply.github.com>
2020-03-13 20:27:34 +0200

    Update TEST_ITEMS.py

--- TEST_ITEMS.py
+++ TEST_ITEMS.py
@@ -2,7 +2,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_cart_button_is_displayed(browser):
     browser.get(link)
     time.sleep(10)
     assert browser.find_element_by_class_name('btn-add-to-basket').is_element_present(), \
