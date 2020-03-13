45fb61652762a6ae15fb968d6ae6449f5d991c7e
khalustetiana <tetiana.khalus@siteimage.com.ua>
2020-03-13 19:42:45 +0200

    test_items added

---
+++ TEST_ITEMS.py
@@ -0,0 +1,9 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    time.sleep(10)
+    assert browser.find_element_by_class_name('btn-add-to-baske').is_element_present(), \
+        'Кнопка добавления товара в корзину отсутсвует'
